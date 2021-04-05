1c441d9a72df7bbf00d337ecbad6bbc1e3ec915a
Benjamin Ghis <benjamin.ghis@avid.com>
2021-04-05 23:47:54 +0000

    arm template update

---
+++ Avid_Edit_In_The_Cloud_Arm/scripts/create_disk_from_snapshot_v0.1.ps1
@@ -0,0 +1,43 @@
+<#
+    Change the variables to match the desired environment.
+    Do not include the carats, e.g. "<change-me>" should end up with a value like "changed"
+#>
+
+#Provide the subscription Id
+$subscriptionId = "949ce875-4a5d-4f96-8db1-4bbd2f68f6e8"
+#Provide the name of your resource group with the snapshot
+$resourceGroupName = "poc-rg"
+#Provide the name of the snapshot that will be used to create OS disk
+$snapshotName = "testsnap"
+# Change this for the desired VM OS Disk naming convention. 
+# A number and "osdisk" label will be appended in the loop below
+# For MediaComposer VM, replace xxxx with showCode. 
+$prefix = "show-edit"
+#Start number for naming. E.g. if 1, the first disk will be <name>-01-osdisk
+$vmStartIndex = 1
+<#
+    Last number for naming. E.g. if 10, the last disk will be <name>-10-osdisk
+    If the start index is 1 and end index is 10, then 10 disks will be created 1-10
+    If the start index is 11 and end index is 40, then 30 disks will be created 11-40
+#>
+$vmEndIndex = 2
+
+
+##################### Nothing to change below here ###########################
+#Set the context to the subscription Id where Managed Disk will be created
+
+az account set --subscription $SubscriptionId
+
+#The below loop will create the set number of VM clones.
+for($i = $vmStartIndex; $i -le $vmEndIndex; $i += 1) {
+    #Name of the OS disk that will be created using the snapshot
+    $osDiskName = "$prefix-os-disk-{0:d2}" -f $i
+
+    Write-Host "Creating Disk $osDiskName"
+
+    $snapshot = az snapshot show -g $resourceGroupName -n $snapshotName
+
+    $snapshotobject = $snapshot | ConvertFrom-Json
+ 
+    $disk = az disk create --resource-group $resourceGroupName --location $snapshotobject.location --sku "Premium_LRS" --source $snapshotobject.id -n $osDiskName
+}
